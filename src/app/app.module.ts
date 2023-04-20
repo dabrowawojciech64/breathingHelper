a1d86b95fd7add6cce20598850e275d4a0d80222
dabrowawojciech64 <51543327+dabrowawojciech64@users.noreply.github.com>
2023-04-20 20:14:58 +0200

    First version of breathing animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +12,9 @@ import { BreatherModule } from './breather/breather.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BreatherModule
+    BreatherModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/breather/breather/breather.component.ts
+++ src/app/breather/breather/breather.component.ts
@@ -1,15 +1,62 @@
+import { state, trigger, animate, style, transition, group, query, animateChild } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-breather',
   templateUrl: './breather.component.html',
-  styleUrls: ['./breather.component.scss']
+  styleUrls: ['./breather.component.scss'],
+  animations: [
+    trigger('outerCircleAnimation', [
+      state('empty',
+        style({
+          transform: 'scale(20%)'
+        })
+      ),
+      state('full',
+        style({
+          transform: 'scale(100%)'
+        }),
+      ),
+
+      transition('void => *', [
+        query('.inner-circle',style({transform: 'scale(50%)'}),{optional:true})
+      ]),
+
+      transition('empty => full', [
+        group([
+          animate('{{inhaleTime}}ms {{exhaleHoldTime}}ms ease-in-out'),
+          query('.inner-circle',
+            animate('{{inhaleTime}}ms {{exhaleHoldTime}}ms ease-in-out', style({ transform: 'scale(100%)' })),
+            {optional: true}
+          )
+      ])
+      ]),
+
+      transition('full => empty', [
+        group([
+          animate('{{exhaleTime}}ms {{inhaleHoldTime}}ms ease-in-out'),
+          query('.inner-circle',
+            animate('{{exhaleTime}}ms {{inhaleHoldTime}}ms ease-in-out', style({ transform: 'scale(50%)' })),
+          {optional:true})
+        ])
+      ])
+    ])
+  ]
 })
 export class BreatherComponent implements OnInit {
+
+  public isInhaling: boolean = false;
+  public inhaleTime: number = 4000;
+  public inhaleHoldTime: number = 4000;
+  public exhaleTime: number = 4000;
+  public exhaleHoldTime: number = 4000;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggleState(event: any): void {
+    this.isInhaling = !this.isInhaling;
+  }
 }

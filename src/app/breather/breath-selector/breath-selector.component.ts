5abe64b54d85f7222a0bd395f57bd0e0a2b518fe
dabrowawojciech64 <51543327+dabrowawojciech64@users.noreply.github.com>
2023-04-24 01:18:10 +0200

    Prepare breathSelector as a form

--- src/app/breather/breath-selector/breath-selector.component.ts
+++ src/app/breather/breath-selector/breath-selector.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-breath-selector',
   templateUrl: './breath-selector.component.html',
-  styleUrls: ['./breath-selector.component.scss']
+  styleUrls: ['./breath-selector.component.scss'],
 })
 export class BreathSelectorComponent {
+  breathSelectorForm = new FormGroup({
+    inhaleTime: new FormControl(null, Validators.required),
+    inhaleHoldTime: new FormControl(null),
+    exhaleTime: new FormControl(null, Validators.required),
+    exhaleHoldTime: new FormControl(null),
+  });
 
+  public onSubmit() {
+    console.warn(this.breathSelectorForm.value);
+  }
 }
--- src/app/breather/breather.module.ts
+++ src/app/breather/breather.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BreatherComponent } from './breather/breather.component';
 import { BreathSelectorComponent } from './breath-selector/breath-selector.component';
 import { MaterialModule } from '../shared/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BreatherComponent, BreathSelectorComponent],
   exports: [BreatherComponent, BreathSelectorComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
 })
 export class BreatherModule {}
